51f048d308811f51760243064ca385bcf724970e
tagtart1 <adamtbizz@gmail.com>
2023-12-15 23:57:22 -0600

    Fix db connections

--- backend/db.js
+++ backend/db.js
@@ -1,53 +1,53 @@
-require("dotenv").config();
-
 const Pool = require("pg").Pool;
 const AppError = require("./utils/appError");
 const fs = require("fs");
 const path = require("path");
-
 const {
   SecretsManagerClient,
   GetSecretValueCommand,
 } = require("@aws-sdk/client-secrets-manager");
 
 const secret_name = "prod/chimps/postgres";
+const client = new SecretsManagerClient({ region: "us-east-1" });
+let pool;
 
-const client = new SecretsManagerClient({
-  region: "us-east-1",
-});
+async function initializePool() {
+  let response;
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT",
+      })
+    );
 
-let response;
+    const secret = JSON.parse(response.SecretString);
+    const sslCert = fs.readFileSync(
+      path.join("/home/ubuntu", "chimps-global-bundle.pem")
+    );
 
-try {
-  response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-    })
-  );
-} catch (error) {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-
-  throw new AppError("Server Error", 500, "SERVER_ERROR");
+    pool = new Pool({
+      user: secret.username,
+      host: secret.host,
+      database: secret.dbname,
+      password: secret.password,
+      port: secret.port,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: sslCert,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw new AppError("Server Error", 500, "SERVER_ERROR");
+  }
 }
 
-const secret = JSON.parse(response.SecretString);
+function getPool() {
+  if (!pool) {
+    throw new Error("Database pool has not been initialized.");
+  }
+  return pool;
+}
 
-const sslCert = fs.readFileSync(
-  path.join("/home/ubuntu", "chimps-global-bundle.pem")
-);
-
-const pool = new Pool({
-  user: secret.username,
-  host: secret.host,
-  database: secret.dbname,
-  password: secret.password,
-  port: secret.port,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: sslCert,
-  },
-});
-
-module.exports = pool;
+module.exports = { initializePool, getPool };
