be38668d6254c514f95bb88623fb802ca29631f2
tagtart1 <adamtbizz@gmail.com>
2023-12-26 16:33:59 -0600

    Move key fetch code

--- backend/middleware/validateToken.js
+++ backend/middleware/validateToken.js
@@ -5,6 +5,7 @@ const fetchJwtSecret = require("../utils/fetchJwtSecret");
 // Verified and grabs user payload from JWT
 const validateToken = async (req, res, next) => {
   const token = req.cookies.token;
+  const key = await fetchJwtSecret();
   // maybe bug: need to see if this is actually returning an error
   if (!token) {
     throw new AppError(
@@ -13,7 +14,7 @@ const validateToken = async (req, res, next) => {
       "SESSION_INVALID"
     );
   }
-  const key = await fetchJwtSecret();
+
   jwt.verify(token, key, (err, data) => {
     if (err)
       throw new AppError(
