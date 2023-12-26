bda51f43aeb09994fb22996fd1998855141f5b23
tagtart1 <adamtbizz@gmail.com>
2023-12-26 16:51:24 -0600

    Handle async validateToken correctly

--- backend/middleware/validateToken.js
+++ backend/middleware/validateToken.js
@@ -4,27 +4,35 @@ const fetchJwtSecret = require("../utils/fetchJwtSecret");
 
 // Verified and grabs user payload from JWT
 const validateToken = async (req, res, next) => {
-  const token = req.cookies.token;
-  const key = await fetchJwtSecret();
-  // maybe bug: need to see if this is actually returning an error
-  if (!token) {
-    throw new AppError(
-      "Session timed out. Please sign up or log in, check 1",
-      401,
-      "SESSION_INVALID"
-    );
-  }
-
-  jwt.verify(token, key, (err, data) => {
-    if (err)
-      throw new AppError(
-        "Session timed out. Please sign up or log in, check 2",
-        401,
-        "SESSION_INVALID"
+  try {
+    const token = req.cookies.token;
+    const key = await fetchJwtSecret();
+    // maybe bug: need to see if this is actually returning an error
+    if (!token) {
+      return next(
+        new AppError(
+          "Session timed out. Please sign up or log in, check 1",
+          401,
+          "SESSION_INVALID"
+        )
       );
-    req.user = data.user;
-    next();
-  });
+    }
+
+    jwt.verify(token, key, (err, data) => {
+      if (err)
+        return next(
+          new AppError(
+            "Session timed out. Please sign up or log in, check 1",
+            401,
+            "SESSION_INVALID"
+          )
+        );
+      req.user = data.user;
+      next();
+    });
+  } catch {
+    next(new AppError("Internal Server Error.", 400, "SERVER_ERROR"));
+  }
 };
 
 module.exports = validateToken;
