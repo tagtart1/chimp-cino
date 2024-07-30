a6bffddc102074c45db7a0e940232ca9e55643b9
tagtart1 <adamtbizz@gmail.com>
2024-07-29 22:38:04 -0500

    Finish mines play button design

--- frontend/src/Components/MinesPage/MinesBetControls/MinesBetControls.jsx
+++ frontend/src/Components/MinesPage/MinesBetControls/MinesBetControls.jsx
@@ -6,12 +6,28 @@ import MinesAmountInput from "./MinesAmountInput";
 const MinesBetControls = () => {
   const [betAmount, setBetAmount] = useState();
   const [minesAmount, setMinesAmount] = useState(3);
+  // TODO: change to only save a loadedMines state, if we load in mines, that means we either have a game already started or just started so display the other UI
+  const [started, setStarted] = useState(false);
+
+  const playGame = () => {
+    // Call to api to start a game
+
+    setStarted(true);
+  };
 
   return (
     <section className="mines-bet-controls">
-      <MinesBetInput setBetAmount={setBetAmount} loadedBet={0} />
-      <MinesAmountInput setMinesAmount={setMinesAmount} loadedMines={0} />
-      <button className="play-mines-button">Play</button>
+      {!started ? (
+        <>
+          <MinesBetInput setBetAmount={setBetAmount} loadedBet={0} />
+          <MinesAmountInput setMinesAmount={setMinesAmount} loadedMines={0} />
+          <button className="play-mines-button" onClick={playGame}>
+            Play
+          </button>
+        </>
+      ) : (
+        <div></div>
+      )}
     </section>
   );
 };
