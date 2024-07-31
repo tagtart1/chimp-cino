659479b67cc7f0c4ee12ea1b6673913a512a268a
tagtart1 <adamtbizz@gmail.com>
2024-07-31 17:10:02 -0500

    Finish gem reveal animation

--- frontend/src/Components/MinesPage/MinesGrid/MinesGrid.jsx
+++ frontend/src/Components/MinesPage/MinesGrid/MinesGrid.jsx
@@ -4,36 +4,43 @@ import "./MinesGrid.scss";
 const MinesGrid = () => {
   const revealCell = (e) => {
     const cell = e.target;
-    cell.className.add("test");
+    cell.classList.add("expand-cell");
+    let resultRetrived = true;
+    let isGem = true;
+
+    // Fetch cell result
+
+    // Once the cell is fully expanded and the result has been fetched, reveal the gem or mine
+    cell.addEventListener(
+      "animationend",
+      () => {
+        if (resultRetrived) {
+          cell.classList.add("shrink-cell");
+
+          cell.addEventListener(
+            "animationend",
+            () => {
+              if (isGem) {
+                cell.parentElement.classList.add("reveal-gem");
+              } else {
+                // Reveal a mine
+              }
+            },
+            { once: true }
+          );
+        }
+      },
+      { once: true }
+    );
   };
 
   return (
     <div className="mines-grid">
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell" onClick={revealCell}></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
-      <div className="mine-cell"></div>
+      {[...Array(25)].map((_, index) => (
+        <button className="cell-wrapper">
+          <div className="cell" onClick={revealCell}></div>
+        </button>
+      ))}
     </div>
   );
 };
