05ebb9bb0a9b69489e895f8d7c448dcfcb626dd8
tagtart1 <adamtbizz@gmail.com>
2024-08-09 00:22:54 -0500

    Add the post and get route setup to create and fetch a mines game

--- backend/routes/mines.js
+++ backend/routes/mines.js
@@ -3,4 +3,10 @@ var router = express.Router();
 const minesController = require("../controllers/minesController");
 const validateToken = require("../middleware/validateToken");
 
+// Create a new mines game
+router.post("/games");
+
+// Fetch an active mines game
+router.get("/games");
+
 module.exports = router;
